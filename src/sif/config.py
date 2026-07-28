fcd449f18b28c7dca685e842e917156e11fabc75
ChaPop870 <183779667+ChaPop870@users.noreply.github.com>
2026-07-28 14:19:55 +0200

    Added data paths to config.py

--- src/sif/config.py
+++ src/sif/config.py
@@ -1,4 +1,13 @@
+from pathlib import Path
+
+
+ROOT = Path(__file__).parents[2]
+
+DATA_DIR = Path("data")
+
+ZIP_DIR = ROOT / DATA_DIR
+
+
 class Constants:
     """Object that holds constants necessary for the project."""
     ...
-

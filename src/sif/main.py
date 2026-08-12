b14f9fb5df91e4d230966e0652559b308e08932f
drowe <daniel.rowe9193@gmail.com>
2026-08-12 11:25:18 +0200

    Update main.py to reflect changes.

--- src/sif/main.py
+++ src/sif/main.py
@@ -1,12 +1,20 @@
 import xarray as xr
 
+from plot import FehmarnRadiosondePlotter
 from radiosondes import Radiosonde
 
+stem = "RS_TestData_Westermarkelsdorf_20220825_131825"
+
 radiosonde = Radiosonde(
-    filepath="../../data/Westermarkelsdorf_RS92_20260811_114920.mwx"
+    filepath=f"../../data/{stem}.mwx"
 )
 radiosonde.extract_mwx()
 radiosonde.build_radiosonde()
 
-ds = xr.open_dataset('../../data/Westermarkelsdorf_RS92_20260811_114920.nc')
-print(ds.data_vars)
+ds = xr.open_dataset(f'../../data/{stem}.nc')
+print(ds)
+
+plotter = FehmarnRadiosondePlotter(
+    filepath=f'../../data/{stem}.nc'
+)
+plotter.plot_skewt()

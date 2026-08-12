b607e81d6b6af1d82f74292e1f56218121bd25bc
drowe <daniel.rowe9193@gmail.com>
2026-08-12 11:38:08 +0200

    Update main.py to reflect changes.

--- src/sif/main.py
+++ src/sif/main.py
@@ -5,15 +5,18 @@ from radiosondes import Radiosonde
 
 stem = "RS_TestData_Westermarkelsdorf_20220825_131825"
 
+# Construct radiosonde from .mwx data.
 radiosonde = Radiosonde(
     filepath=f"../../data/{stem}.mwx"
 )
 radiosonde.extract_mwx()
 radiosonde.build_radiosonde()
+radiosonde.summarize()
 
+# Can load radiosonde data as xarray dataset, if desired.
 ds = xr.open_dataset(f'../../data/{stem}.nc')
-print(ds)
 
+# Plot a skew-t diagram of the radiosonde.
 plotter = FehmarnRadiosondePlotter(
     filepath=f'../../data/{stem}.nc'
 )

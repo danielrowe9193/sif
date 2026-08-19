ac96f628a1925f955eecbcd9127cade544be6e6f
drowe <daniel.rowe9193@gmail.com>
2026-08-19 15:00:57 +0200

    feat: add stations to config.Constants.

--- src/sif/config.py
+++ src/sif/config.py
@@ -1,11 +1,9 @@
-from pathlib import Path
-
-
-ROOT = Path(__file__).parents[2]
-
-DATA_DIR = ROOT / Path("data")
-
-
 class Constants:
     """Object that holds constants necessary for the project."""
-    ...
+
+    STATIONS = {
+        "nordeney": {"latitude": 53.712, "longitude": 7.152},
+        "schleswig": {"latitude": 54.528, "longitude": 9.55},
+        "fehmarn": {"latitude": 54.527846, "longitude": 11.060437},
+        "griefswald": {"latitude": 54.097, "longitude": 13.405},
+    }

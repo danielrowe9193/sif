7e4e596e2ac62f7d8ac9e791c7d80e90a80721a6
drowe <daniel.rowe9193@gmail.com>
2026-08-26 18:04:39 +0200

    refactor: update main.py to reflect changes.

--- src/sif/main.py
+++ src/sif/main.py
@@ -2,34 +2,17 @@ import src.sif.utils.plot as plot
 import src.sif.observations.radiosondes as rs
 
 from src.sif.models import forecast_radiosondes
-from src.sif.utils.utils import FileManagement
-from pathlib import Path
+from src.sif.utils.utils import FileManagement as fm
 
 # Rename files so there are sorted by date.
-FileManagement.rename_mwx(FileManagement.MWX_DIR)
+fm.rename_mwx(fm.MWX_DIR)
 
-# Read radiosondes and build level0 to level2 datasets.
-radiosondes = rs.Radiosondes(
-    mwx_dir=Path("../data/mwx/")
+# Read radiosondes and build level0 to level2 datasets. Save the datasets.
+radiosondes_pipeline = rs.RadiosondePipeline(
+    mwx_dir=fm.MWX_DIR
 )
-
-lvl0 = rs.RadiosondesLevel0(
-    radiosondes=radiosondes
-)
-lvl0.build_ptu_radiosondes_lvl0()
-lvl0.build_std_plvl_radiosondes_lvl0()
-
-lvl1 = rs.RadiosondesLevel1(
-    radiosondes_lvl0=lvl0
-)
-lvl1.build_ptu_radiosondes_lvl1()
-lvl1.build_std_plvl_radiosondes_lvl1()
-
-lvl2 = rs.RadiosondesLevel2(
-    radiosondes_lvl1=lvl1
-)
-lvl2.build_ptu_radiosondes_lvl2()
-lvl2.build_std_plvl_radiosondes_lvl2()
+radiosondes_pipeline.run_ptu_pipeline()
+radiosondes_pipeline.run_std_plvl_pipeline()
 
 # Create IFS level-0 and level-1 datasets
 ifs_level_zero = forecast_radiosondes.IFSLevelZero()
@@ -57,5 +40,5 @@ plotter = plot.FehmarnRadiosondeIndicesPlotter(
     sif_filepath="../../data/netcdf/sif.radiosondes.profiles.nc",
     ifs_filepath="../../data/ifs/ifs.radiosondes.profiles.level1.nc"
 )
-plotter.plot_indices_over_time()
+# plotter.plot_indices_over_time()
 

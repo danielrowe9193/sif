corrected lat and lon data, fixed pressure units

src/sif/clean_icon.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 from datetime import datetime
+import numpy as np
 import xarray as xr
 import pandas as pd
 
@@ -28,12 +29,27 @@ def get_date():
 # select stations
 def select_station(data, stations):
     selected = []
-    for name, lat, lon in stations:
-        point = data.sel(
-            latitude=lat,
-            longitude=lon,
-            method="nearest"
+    lat = data["latitude"].values
+    lon = data["longitude"].values
+
+    for name, station_lat, station_lon in stations:
+        station_lat = np.deg2rad(station_lat)
+        station_lon = np.deg2rad(station_lon)
+
+        dlat = lat - station_lat
+        dlon = (lon - station_lon + np.pi) % (2 * np.pi) - np.pi
+        # calculate spherical distance
+        a = (
+            np.sin(dlat / 2) ** 2
+            + np.cos(station_lat)
+            * np.cos(lat)
+            * np.sin(dlon / 2) ** 2
         )
+
+        distance = 2 * np.arcsin(np.sqrt(a))
+        cell_idx = np.nanargmin(distance)
+        point = data.isel(cell=cell_idx)
+
         # add station as another dimension
         point = point.expand_dims(station=[name])
         selected.append(point)
@@ -64,39 +80,50 @@ for cycle in cycles:
     input_path = BASE_PATH / f"id2_lex2026_{date_no_dash}{cycle}.nc"
     ds = xr.open_dataset(input_path)
 
-    # rename levels and cells
-    clean_ds = ds.rename({
-        # "height": "level",
-        "ncells": "cell",
-    })
-
     # read lat long info with cooresponding cells
     cells = pd.read_csv(
         cell_file,
         sep=r"\s+"
     )
-    cells["cell_index"] = cells["cell"]
+
+    # rename levels and cells
+    clean_ds = ds.rename({
+        # "height": "level",
+        "ncells": "cell",
+    })
+# clean_ds = xr.Dataset(
+#     data_vars={
+#         "t": ds["T"].rename({"ncells": "cell"}),
+#         "p": ds["P"].rename({"ncells": "cell"}),
+#     },
+
 
     clean_ds = xr.Dataset(
         data_vars={ #select variables
-            "t": ds["T"], #temperature
-            "p": ds["P"], #pressure # in Pa -> change to hPa?
-            "q": ds["QV"], # specific humidity
-            "rh": ds["RELHUM_2M"], # relative humidity
-            "td": ds["TD_2M"], #dew point temperature (2m)
-            "U": ds["U"],
-            "V": ds["V"],
-            "CAPE_ML": ds["CAPE_ML"],
-            "CIN_ML": ds["CIN_ML"],
+            "t": ds["T"].rename({"ncells": "cell"}), #temperature
+            "p": ds["P"].rename({"ncells": "cell"}), #pressure # in Pa 
+            "q": ds["QV"].rename({"ncells": "cell"}), # specific humidity
+            "rh": ds["RELHUM_2M"].rename({"ncells": "cell"}), # relative humidity
+            "td": ds["TD_2M"].rename({"ncells": "cell"}), #dew point temperature (2m)
+            "U": ds["U"].rename({"ncells": "cell"}),
+            "V": ds["V"].rename({"ncells": "cell"}),
+            "CAPE_ML": ds["CAPE_ML"].rename({"ncells": "cell"}),
+            "CIN_ML": ds["CIN_ML"].rename({"ncells": "cell"}),
         },
         coords={
-            # "valid_time": ds["time"],
+            "valid_time": ds["time"],
             # "level": ds["height"],
-            "cells": ds["ncells"],
+            "cell": np.arange(len(cells)),
             "latitude": ("cell", cells["lat"].values,),
             "longitude": ("cell", cells["lon"].values,),
         },
     )
+
+    # change to hPa
+    clean_ds["p"] = clean_ds["p"] / 100
+    clean_ds["p"].attrs["units"] = "hPa"
+
+    clean_ds = select_station(clean_ds, stations)
 
     output = output_path / f"ALL-{date_input}-{cycle}z.nc"
     clean_ds.to_netcdf(output)